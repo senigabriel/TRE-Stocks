b3fa42b55585896f66c9dd55e3fbf99b81418717
senigabriel <126727741+senigabriel@users.noreply.github.com>
2026-02-24 19:11:47 -0500

    Update infra.ts

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { IngestionStack } from '../lib/ingestion-stack';
-// We’ll add these later
-// import { ApiStack } from '../lib/api-stack';
-// import { FrontendStack } from '../lib/frontend-stack';
 
 const app = new cdk.App();
 
 new IngestionStack(app, 'TreStocksIngestionStack');
-
-// These will be enabled as we build them
-// new ApiStack(app, 'TreStocksApiStack');
-// new FrontendStack(app, 'TreStocksFrontendStack');
