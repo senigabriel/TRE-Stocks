4046303b629adae405092ae32e2fdd57e539200f
senigabriel <126727741+senigabriel@users.noreply.github.com>
2026-03-09 14:56:36 -0400

    Update dynamodb.ts

--- infra/lib/shared/dynamodb.ts
+++ infra/lib/shared/dynamodb.ts
@@ -1,20 +1,22 @@
 import { Construct } from 'constructs';
-import {
-  Table,
-  AttributeType,
-  BillingMode,
-} from 'aws-cdk-lib/aws-dynamodb';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
-export class MoversTable extends Construct {
-  public readonly table: Table;
+export class MoversTable {
+  public readonly table: dynamodb.Table;
 
   constructor(scope: Construct, id: string) {
-    super(scope, id);
+    this.table = new dynamodb.Table(scope, id, {
+      partitionKey: { name: 'symbol', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'date', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
-    this.table = new Table(this, 'MoversTable', {
-      partitionKey: { name: 'date', type: AttributeType.STRING },
-      billingMode: BillingMode.PAY_PER_REQUEST,
+   
+    this.table.addGlobalSecondaryIndex({
+      indexName: 'GSI1',
+      partitionKey: { name: 'date', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'symbol', type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
     });
   }
 }
-
