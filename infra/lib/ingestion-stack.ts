0b976b62f9bda4fca985f06f4c9f91b9c22ef3a0
senigabriel <126727741+senigabriel@users.noreply.github.com>
2026-03-10 12:29:35 -0400

    Update ingestion-stack.ts

--- infra/lib/ingestion-stack.ts
+++ infra/lib/ingestion-stack.ts
@@ -1,39 +1,45 @@
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { MoversTable } from './shared/dynamodb';
 import { dynamoWritePolicy } from './shared/iam';
 
+interface IngestionStackProps extends StackProps {
+  moversTable: MoversTable;
+}
+
 export class IngestionStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: IngestionStackProps) {
     super(scope, id, props);
 
-    const moversTable = new MoversTable(this, 'MoversTable');
+    const moversTable = props.moversTable;
 
     const ingestionLambda = new lambda.Function(this, 'IngestionLambda', {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'handler.handler',
       code: lambda.Code.fromAsset('../lambdas/ingestion'),
+
+      timeout: Duration.seconds(90),
+      memorySize: 512,
+
       environment: {
         TABLE_NAME: moversTable.table.tableName,
-	MASSIVE_API_KEY: process.env.MASSIVE_API_KEY ?? 'PLACEHOLDER',
+        MASSIVE_API_KEY: process.env.MASSIVE_API_KEY ?? 'PLACEHOLDER',
       },
     });
 
-//event bridge rule
-	new events.Rule(this, 'DailyStockIngestionRule', {
-  	schedule: events.Schedule.cron({
-    	minute: '0',
-    	hour: '22', // 10 PM UTC ≈ market closed in US
-  	}),
-  	targets: [new targets.LambdaFunction(ingestionLambda)],
-	});
+    //EventBridge schedule (daily)
+    new events.Rule(this, 'DailyStockIngestionRule', {
+      schedule: events.Schedule.cron({
+        minute: '0',
+        hour: '22', // 10 PM UTC ≈ market closed US
+      }),
+      targets: [new targets.LambdaFunction(ingestionLambda)],
+    });
 
-    ingestionLambda.addToRolePolicy(
-      dynamoWritePolicy(moversTable.table)
-    );
+    //DynamoDB write permissions
+    ingestionLambda.addToRolePolicy(dynamoWritePolicy(moversTable.table));
   }
 }
-
